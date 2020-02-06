149a2ed11c057982e956bf4f8ad7b328b40d17a0
Robbin de Groot <robbindegroot@gmail.com>
2020-02-06 09:59:56 +0100

    unzip

---
+++ Ibrahimi_deGroot_OpenGL_0/Code/mainview.cpp
@@ -0,0 +1,41 @@
+#include "mainview.h"
+
+MainView::MainView(QWidget *parent) : QOpenGLWidget(parent) {
+    qDebug() << "MainView constructor";
+}
+
+MainView::~MainView() {
+    qDebug() << "MainView destructor";
+
+    makeCurrent();
+}
+
+void MainView::initializeGL() {
+    qDebug() << ":: Initializing OpenGL";
+    initializeOpenGLFunctions();
+
+    connect(&debugLogger, SIGNAL(messageLogged(QOpenGLDebugMessage)),
+            this, SLOT(onMessageLogged(QOpenGLDebugMessage)), Qt::DirectConnection);
+
+    if (debugLogger.initialize()) {
+        qDebug() << ":: Logging initialized";
+        debugLogger.startLogging(QOpenGLDebugLogger::SynchronousLogging);
+    }
+
+    QString glVersion;
+    glVersion = reinterpret_cast<const char*>(glGetString(GL_VERSION));
+    qDebug() << ":: Using OpenGL" << qPrintable(glVersion);
+}
+
+void MainView::resizeGL(int newWidth, int newHeight) {
+    Q_UNUSED(newWidth)
+    Q_UNUSED(newHeight)
+}
+
+void MainView::paintGL() {
+
+}
+
+void MainView::onMessageLogged(QOpenGLDebugMessage Message) {
+    qDebug() << " → Log:" << Message;
+}
