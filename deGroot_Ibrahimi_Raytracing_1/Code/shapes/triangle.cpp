f3f5f19e24ecb48ca2571dc7fab6f4e61fa96a8f
Erblin Ibrahimi <erblinibrahimi@gmail.com>
2020-02-27 09:37:07 +0100

    Fix merge conflict

--- deGroot_Ibrahimi_Raytracing_1/Code/shapes/triangle.cpp
+++ deGroot_Ibrahimi_Raytracing_1/Code/shapes/triangle.cpp
@@ -3,25 +3,46 @@
 #include <cmath>
 
 Hit Triangle::intersect(Ray const& ray) {
-    double epsilon = 0.00001;
+//    double epsilon = 0.00001;
+//
+//    // Check for ray parallel to triangle
+//    if (epsilon > ray.D.dot(N) && -epsilon < ray.D.dot(N)) return Hit::NO_HIT();
+//
+//    // Calculate t
+//    double t = (N.dot(v0) - N.dot(ray.O)) / (N.dot(ray.D));
+//    Point X = ray.at(t);
+//
+//    // Calculate alpha and beta such that X = P + alpha * R + beta * Q
+//    Point P = v0, R = v2 - v0, Q = v1 - v0;
+//    if (fabs(Q.x * R.y - R.x * Q.y) < epsilon || fabs(Q.x) < epsilon) return Hit::NO_HIT();
+//
+//    double beta = (Q.x * (X.y - P.y) + Q.y * (P.x - X.x)) / (Q.x * R.y - R.x * Q.y);
+//    double alpha = (X.x - P.x - beta * R.x) / Q.x;
+//
+//    if (alpha < 0.0 || beta < 0.0 || alpha + beta > 1.0 + 0.0) return Hit::NO_HIT();
+//
+//    return t >= 0 ? Hit(t, N) : Hit::NO_HIT();
 
-    // Check for ray parallel to triangle
-    if (epsilon > ray.D.dot(N) && -epsilon < ray.D.dot(N)) return Hit::NO_HIT();
+    double f = ray.D.dot(N);
+    if (f == 0) {
+        return Hit::NO_HIT();
+    }
 
-    // Calculate t
-    double t = (N.dot(v0) - N.dot(ray.O)) / (N.dot(ray.D));
-    Point X = ray.at(t);
+    double d = v0.dot(N);
 
-    // Calculate alpha and beta such that X = P + alpha * R + beta * Q
-    Point P = v0, R = v2 - v0, Q = v1 - v0;
-    if (fabs(Q.x * R.y - R.x * Q.y) < epsilon || fabs(Q.x) < epsilon) return Hit::NO_HIT();
+    double t = ((d * N) - ray.O).dot(N) / f;
 
-    double beta = (Q.x * (X.y - P.y) + Q.y * (P.x - X.x)) / (Q.x * R.y - R.x * Q.y);
-    double alpha = (X.x - P.x - beta * R.x) / Q.x;
+    if (t < 0) {
+        return Hit::NO_HIT();
+    }
 
-    if (alpha < 0.0 || beta < 0.0 || alpha + beta > 1.0 + 0.0) return Hit::NO_HIT();
+    Point P = ray.O + t * ray.D;
 
-    return t >= 0 ? Hit(t, N) : Hit::NO_HIT();
+    if ((v1 - v0).cross(P - v0).dot(N) < 0) return Hit::NO_HIT();
+    if ((v2 - v1).cross(P - v1).dot(N) < 0) return Hit::NO_HIT();
+    if ((v0 - v2).cross(P - v2).dot(N) < 0) return Hit::NO_HIT();
+
+    return {t, N};
 }
 
 Triangle::Triangle(Point const& v0,
