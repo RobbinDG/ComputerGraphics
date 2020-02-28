549925e03697e083b5d4731ff8f052f44d16dc26
Robbin de Groot <robbindegroot@gmail.com>
2020-02-28 17:47:02 +0100

    Finished Raytracer

--- deGroot_Ibrahimi_Raytracing_1/Code/shapes/cylinder.cpp
+++ deGroot_Ibrahimi_Raytracing_1/Code/shapes/cylinder.cpp
@@ -1,39 +1,23 @@
 #include "cylinder.h"
 
 #include <cmath>
+#include <iostream>
 
 using namespace std;
 
 Hit Cylinder::intersect(Ray const& ray) {
+    // Map direction axis to a vector
     Vector S;
     if (direction.x > 0) S = Vector(0, 1, 1);
     else if (direction.y > 0) S = Vector(1, 0, 1);
     else S = Vector(1, 1, 0);
 
-    double a = S.dot(ray.D * ray.D);
-    double b = 2 * S.dot((ray.O - position) * ray.D);
-    double c = S.dot((ray.O - position) * (ray.O - position)) - radius * radius;
-    double det = b * b - 4 * a * c;
+    // Hits cylinder either on side or top/bottom, choose the closest.
+    Hit hit_side = intersect_side(ray, S);
+    Hit hit_tops = intersect_tops(ray, S);
 
-    double t;
-    if (det > 0.0) {
-        double t1, t2;
-        t1 = (-b + sqrt(det))/ 2 * a;
-        t2 = (-b - sqrt(det))/ 2 * a;
-
-        if (t1 < 0.0 && t2 < 0.0) return Hit::NO_HIT();
-        if (t1 < 0.0) t = t2;
-        else if (t2 < 0.0) t = t1;
-        else t = min(t1, t2);
-    } else if (det == 0.0) {
-        t = (-b + sqrt(det))/ 2 * a;
-    } else {
-        return Hit::NO_HIT();
-    }
-
-    Point X = ray.at(t);
-    Vector N = (X - position).normalized();
-    return {t, N};
+    if (isnan(hit_side.t)) return hit_tops;
+    return hit_side;
 }
 
 Cylinder::Cylinder(Point const& pos, Vector const& direction, double radius)
@@ -41,3 +25,59 @@ Cylinder::Cylinder(Point const& pos, Vector const& direction, double radius)
         position(pos),
         direction(direction),
         radius(radius) {}
+
+Hit Cylinder::intersect_side(Ray const& ray, const Vector& S) {
+    // Calculate t
+    double a = S.dot(ray.D * ray.D);
+    double b = S.dot(2 * (ray.O - position) * ray.D);
+    double c = S.dot((ray.O - position) * (ray.O - position)) - radius * radius;
+    double det = b * b - 4 * a * c;
+
+    double t;
+    if (det > 0.0) {
+        double t1, t2;
+        t1 = (-b + sqrt(det)) / (2 * a);
+        t2 = (-b - sqrt(det)) / (2 * a);
+
+        if (t1 < 0.0 && t2 < 0.0) return Hit::NO_HIT();
+        if (t1 < 0.0) t = t2;
+        else if (t2 < 0.0) t = t1;
+        else t = min(t1, t2);
+    } else if (det == 0.0) {
+        t = -b / (2 * a);
+    } else {
+        // No hit on side of cylinder
+        return Hit::NO_HIT();
+    }
+
+    // Calculate intersection in case of hit on side of cylinder
+    Point X = ray.at(t);
+
+    double h = (X - position).dot(direction);
+    if (h > direction.dot(direction) || h < 0.0) return Hit::NO_HIT();
+
+    Vector N = (X - (position * S + X * (Vector(1, 1, 1) - S))).normalized();
+
+    return {t, N};
+}
+
+Hit Cylinder::intersect_tops(Ray const& ray, const Vector& S) {
+    // Calculate normal, is always in a unit direction
+    Vector N = Vector(1, 1, 1) - S;
+
+    // Find t with intersection of 'bottom' side
+    double t = N.dot(position - ray.O) / (N.dot(ray.D));
+    if (t < 0.0) return Hit::NO_HIT();
+    Point X = ray.at(t);
+    if (((X - position) * (X - position)).dot(S) <= radius * radius)
+        return {t, N}; // Normal is flipped in scene.cpp if not visible
+
+    // Find t with intersection of 'top' side
+    t = ((position + direction - ray.O).dot(N)) / (N.dot(ray.D));
+    if (t < 0.0) return Hit::NO_HIT();
+    X = ray.at(t);
+    if (((X - position) * (X - position)).dot(S) <= radius * radius)
+        return {t, N}; // Normal is flipped in scene.cpp if not visible
+
+    return Hit::NO_HIT();
+}
--- deGroot_Ibrahimi_Raytracing_1/Code/shapes/cylinder.h
+++ deGroot_Ibrahimi_Raytracing_1/Code/shapes/cylinder.h
@@ -3,16 +3,19 @@
 
 #include "../object.h"
 
-class Cylinder: public Object
-{
+class Cylinder : public Object {
     Point const position;
     Vector const direction;
     double const radius;
 
-    public:
-        Cylinder(Point const &pos, Vector const &direction, double radius);
+public:
+    Cylinder(Point const& pos, Vector const& direction, double radius);
 
-        virtual Hit intersect(Ray const &ray);
+    virtual Hit intersect(Ray const& ray);
+
+private:
+    Hit intersect_side(Ray const& ray, const Vector& S);
+    Hit intersect_tops(Ray const& ray, const Vector& S);
 };
 
 #endif
